84419c4bf2aa19cd505c68599c4079350df94809
Jhones Bazilio dos Santos <jhones.santos@resourceit.com>
2018-04-25 14:58:01 -0300

    adicionando caixa de texto para enviar string para outra activity

--- Xabiron/app/src/main/java/com/example/resource/xabiron/MainActivity.java
+++ Xabiron/app/src/main/java/com/example/resource/xabiron/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.resource.xabiron;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
         final TextView tvSomeText = (TextView) findViewById(R.id.tv_text1);
         final TextView tvSomeText2 = (TextView) findViewById(R.id.tv_text2);
         Button btn = (Button) findViewById(R.id.bt_butao);
-        Button btn2 = (Button) findViewById(R.id.bt_butao2);
+        Button btnEnviar = (Button) findViewById(R.id.bt_enviar);
         ImageView imagem = (ImageView) findViewById(R.id.img01);
+        final EditText et = (EditText) findViewById(R.id.et_textoEnv);
 
         tvSomeText.setText("alterado");
         tvSomeText2.setText("alterado tambem");
@@ -44,11 +47,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn2.setOnClickListener(new View.OnClickListener() {
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra(MainActivity.S_BLA,"valor mmms" );
+                intent.putExtra(MainActivity.S_BLA, et.getText().toString());
                 startActivity(intent);
             }
         });
